cc90a356ced74eda3d015d857400f0609f7ab876
wylie-u <wylieurig@gmail.com>
2021-03-30 20:35:16 -0600

    updated employee functions

--- index.js
+++ index.js
@@ -49,9 +49,10 @@ const start = () => {
   ],
   })
     .then((answer) => {
+      console.log(answer)
       // choices then go into a switch statement, call each function based on selection from user
       switch (answer.userOption) {
-        case "View All Employees":
+        case "View All Employees?":
           allEmployees();
           break;
         case "View All Employees By Department?":
@@ -66,6 +67,8 @@ const start = () => {
           case "Remove Employee?":
           removeEmployees();
           break;
+          case "Add Role?":
+          addRole();
           case "Update Employee Role?":
           updatedEmpRole();
           break;
@@ -80,7 +83,7 @@ const start = () => {
 };
 // table first, then property within table (ie = employee(table).property
 const allEmployees = () => {
-  connection.query("SELECT employee.first_name, employee.last_name, role.title, department.name, employee.manager_id FROM employee INNER JOIN job_role ON employee.role_id = job_role.role_id", (err, res) => {
+  connection.query("SELECT * FROM employee", (err, res) => {
     if (err) throw err;
 
     // Log all results of the SELECT statement
@@ -90,15 +93,13 @@ const allEmployees = () => {
   });
 };
 
-const employeesByDepartment = () => {
-  connection.query("SELECT employee.first_name, employee.last_name, department.name AS Department FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY employee.id;", (err, res) => {
-    if (err) throw err;
 
-    // Log all results of the SELECT statement
-    console.table(res);
-    // call start function to bring up options
-    start();
-  });
+const employeesByDepartment = () => {
+  connection.query('SELECT * FROM department', (err, results) => {
+      if (err) throw err;
+      console.table(results);
+      start();
+  })
 };
 
 const employeesByRole = () => {
@@ -108,9 +109,42 @@ const employeesByRole = () => {
 
     // Log all results of the SELECT statement
     console.table(res);
+    // call start function to bring up options
+    start();
     
   });
 };
+
+// function to store roles to use for adding employee function
+// empty array will hold the role choice that will be passed in from the user response
+var roleChoice = [];
+function selectRole() {
+  connection.query("SELECT * FROM role", function(err, res) {
+    if (err) throw err
+    for (var i = 0; i < res.length; i++) {
+      // response from the user will be pushed into the empty array 
+      roleChoice.push(res[i].title);
+    }
+
+  })
+  return roleChoice;
+}
+
+// function to store manager choice after query is called 
+// empty array will hold choice after the prompt is ran
+var managerChoice = [];
+function selectManager() {
+  connection.query("SELECT first_name, last_name FROM employee WHERE manager_id IS NULL", function(err, res) {
+    if (err) throw err
+    for (var i = 0; i < res.length; i++) {
+      // response from the user will be pushed into the empty array 
+      managerChoice.push(res[i].first_name);
+    }
+
+  })
+  return managerChoice;
+}
+
 
 
 // add employee
@@ -130,8 +164,8 @@ function addEmployee () {
         {
           name: "role",
           type: "list",
-          message: "What is their role? ",
-          //calls selectRole function
+          message: "What is their role?",
+          //calls selectRole function using the role's query 
           choices: selectRole()
         },
         {
@@ -143,13 +177,21 @@ function addEmployee () {
         }
   ])
   .then((answers) => {
+      var roleId = selectRole().indexOf(answers.role) + 1
+      var managerId = selectManager().indexOf(answers.manager) + 1
+    // answers from the prompt will be set as values of the properties from the table
       connection.query("INSERT INTO employee SET ?", 
       {
+          // first name in employee column
           first_name: answers.firstName,
+          // last name in employee column
           last_name: answers.lastName,
+          // manager id in employee column 
           manager_id: managerId,
+          // role id in employee column
           role_id: roleId
-          
+
+           
       }, function(err){
           if (err) throw err
           console.table(answers)
@@ -159,9 +201,8 @@ function addEmployee () {
   })
 
 }
-
+// add department
 function addDepartment () {
-
   inquirer
   .prompt([
         {
@@ -173,7 +214,7 @@ function addDepartment () {
         .then((answers) => {
           connection.query("INSERT INTO department SET ?", 
           {
-              
+            // ties to name column in department table 
             name: answers.department    
               
           }, function(err){
@@ -184,8 +225,138 @@ function addDepartment () {
     
       })
 
-
 }
 
+function addRole () {
+  inquirer
+  .prompt([
+        {
+            type: 'input',
+            name: 'role',
+            message: 'What is the role you would like to add?'
+        },
+        {
+            type: 'input',
+            name: 'departmentId',
+            message: 'What is the department ID for this role?'
+      },
+      ]).then((answers) => {
+        connection.query("INSERT INTO role SET ?",
+        {
+          // ties to name column in role table 
+          role: answers.role,  
+          // ties to department id
+          department_id: answers.department_id,
 
+            
+        }, function(err){
+            if (err) throw err
+            console.table(answers)
+            start()
 
+        })
+
+})
+}
+
+function removeEmployees () {
+  connection.query("SELECT employee.last_name, role.title FROM employee JOIN role ON employee.role_id = role.id;", function(err, res) {
+    // console.log(res)
+     if (err) throw err
+     console.log(res)
+     inquirer
+  .prompt([
+        {
+            type: 'input',
+            name: 'employee',
+            choices: function() {
+              var lastName = [];
+              for (var i = 0; i < res.length; i++) {
+                lastName.push(res[i].last_name);
+              }
+              return lastName;
+            },
+            message: "What is the Employee's last name? ",
+        },
+      ]).then((answers) => {
+        console.log('Deleted selected employee\n');
+        connection.query('DELETE FROM employee WHERE ?',
+    {
+      last_name: answers.employee,
+    },
+    (err, res) => {
+      if (err) throw err;
+      console.log(`${res.affectedRows} employee removed!\n`);
+      // Call start AFTER the DELETE completes
+      start();
+    }
+  );
+      })
+  })  
+}
+
+function updatedEmpRole () {
+  connection.query("SELECT employee.last_name, role.title FROM employee JOIN role ON employee.role_id = role.id;", function(err, res) {
+    // console.log(res)
+     if (err) throw err
+     console.log(res)
+     // need to reference last name of employee and update role 
+     inquirer
+    .prompt([
+      {
+        name: 'employee',
+        type: 'input',
+        choices: function() {
+          var lastName = [];
+          for (var i = 0; i < res.length; i++) {
+            lastName.push(res[i].last_name);
+          }
+          return lastName;
+        },
+        message: "What is the Employee's last name? ",
+      },
+      {
+
+        name: 'role',
+        type: 'input',
+        message: "What is the Employees new title? ",
+    
+      
+      },
+
+    ]).then((answer) => {
+      
+      let chosenRole;
+      results.forEach((role_id) => {
+        if (role_id.last_name === answer.employee) {
+          chosenRole = role_id;
+        }
+      });
+
+      
+      if (parseInt(answer.role)) {
+       
+        connection.query(
+          'UPDATE employee SET ? WHERE ?',
+          [
+            {
+              role_id: answer.role,
+            },
+            {
+              id: chosenRole.id,
+            },
+          ],
+          (error) => {
+            if (error) throw err;
+            console.log('Employee updated correctly!');
+            start();
+          }
+        );
+      } else {
+        // if error, fix
+        console.log('Problem! Please fix errors!');
+        start();
+      }
+    });
+})
+}
